5ff53df017d938684dbf48bef0a02278548bcb82
nik <kol-3cl@outlook.com>
2020-01-29 14:13:47 +0200

    initial commit

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master', ['file' => 'categories'])
+@section('title', 'Все категории')
+@section('content')
+        @foreach($categories as $category)
+            <div class="panel">
+                <a href="{{route('category', $category->code)}}">
+                    <img src="{{ Storage::url($category->image) }}">
+                    <h2>{{$category->name}}</h2>
+                </a>
+                <p>
+                    {{$category->description}}
+                </p>
+            </div>
+        @endforeach
+@endsection
---
+++ resources/views/product.blade.php
@@ -0,0 +1,20 @@
+
+@extends('layouts.master')
+
+@section('title', 'Товар')
+
+@section('content')
+    <h1>{{ $product->name }}</h1>
+    <h2>{{ $product->category->name }}</h2>
+    <p>Цена: <b>{{ $product->price }} грн.</b></p>
+    <img src="{{ Storage::url($product->image) }}">
+    <p>{{ $product->description }}</p>
+    <form action="{{ route('basket-add', $product) }}" method="POST">
+        @if($product->isAvailable())
+            <button type="submit" class="btn btn-success" role="button">В корзину</button>
+        @else
+            Не доступен
+        @endif
+        @csrf
+    </form>
+@endsection
